84278e923cc3ad32bd365e26a47c716d0a79120d
abhaykatheria <abhay.katheria1998@gmail.com>
2021-03-22 21:25:18 +0530

    asdad

---
+++ team.php
@@ -0,0 +1,229 @@
+<?php
+$title = "Team | Culrav";
+if(($_SERVER['REQUEST_METHOD'] === 'GET') or (isset($_POST['action']) and $_POST['action'] != "getContentPage")){
+    include_once('components/header.php');
+}
+?>
+<style>
+.celeb-image > img{max-height:405px;width:auto;}
+.celeb-image > .glitch-image{filter:grayscale(80%);}
+</style>
+<section style="margin-top:70px;">
+    <div id="about_us" style="margin-bottom:0">
+        <div data-aos="fade-down" data-aos-anchor-placement="top-center"><h1 style="animation:unset;">Without involvement,<br>there is no commitment.</h1><br><p>Despite for all the problems and barriers, the team behind this event who are working hard to make this event possible.</p></div>
+</section>
+
+<section class="celebs">
+    <center style="margin:50px 0"><h1><span>Professor In-charge</span></h1></center>
+    <div class="flex" style="margin-top:0;">
+
+        <div class="celeb">
+            <div class="celeb-image">
+                <div class="glitch-image" style="filter:unset;"><img src="images/team/BCKLESS_PRof_Abhishek.png"></div>
+                <div class="celeb-work">Professor in-charge</div>
+                <div class="celeb-social-links">
+                    <a><i class="fa fa-facebook"></i></a>
+                    <a><i class="fa fa-twitter"></i></a>
+                    <a><i class="fa fa-linkedin"></i></a>
+                </div>
+            </div>
+            <div class="celeb-name"><h3>Prof. Abhisekh</h3></div>
+            <div class="celeb-details"></div>
+        </div>
+
+        <div class="celeb">
+            <div class="celeb-image">
+                <div class="glitch-image" style="filter:unset;"><img src="images/team/BCKLESS_PSAC.png"></div>
+                <div class="celeb-work">Professor in-charge</div>
+                <div class="celeb-social-links">
+                    <a href=""><i class="fa fa-github"></i></a>
+                    <a><i class="fa fa-twitter"></i></a>
+                    <a><i class="fa fa-linkedin"></i></a>
+                </div>
+            </div>
+            <div class="celeb-name"><h3>Prof. Rajesh Gupta</h3></div>
+            <div class="celeb-details"></div>
+        </div>
+
+    </div>
+    <center style="margin:50px 0"><h1><span>Festival Secretary</span></h1></center>
+    <div class="flex" style="margin-top:0;">
+
+        <div class="celeb">
+            <div class="celeb-image">
+                <div class="glitch-image"><img src="images/team/Avinash.png"></div>
+                <div class="celeb-work">Festival Secretary</div>
+                <div class="celeb-social-links">
+                    <a href="https://www.facebook.com/profile.php?id=100005587684536"><i class="fa fa-facebook"></i></a>
+                    <a href="https://www.instagram.com/avi_sharma_._"><i class="fa fa-instagram"></i></a>
+                    <a href="mailto:culrav2021@gmail.com"><i class="fa fa-envelope"></i></a>
+                    <a href="tel:+918918299688"><i class="fa fa-phone"></i></a>
+                </div>
+            </div>
+            <div class="celeb-name"><h3>Avinash Sharma</h3></div>
+        </div>
+
+        <div class="celeb">
+            <div class="celeb-image">
+                <div class="glitch-image"><img src="images/team/Fares.png"></div>
+                <div class="celeb-work">Festival Secretary</div>
+                <div class="celeb-social-links">
+                    <a href="https://www.facebook.com/faresmanzarkhan"><i class="fa fa-facebook"></i></a>
+                    <a href="https://www.instagram.com/faresmanzarkhan/"><i class="fa fa-instagram"></i></a>
+                    <a href="mailto:culrav2021@gmail.com"><i class="fa fa-envelope"></i></a>
+                    <a href="tel:+966571606718"><i class="fa fa-phone"></i></a>
+                </div>
+            </div>
+            <div class="celeb-name"><h3>Fares Manzar Khan</h3></div>
+            <div class="celeb-details"></div>
+        </div>
+
+        <div class="celeb">
+            <div class="celeb-image">
+                <div class="glitch-image"><img src="images/team/Manas.png"></div>
+                <div class="celeb-work">Festival Secretary</div>
+                <div class="celeb-social-links">
+                    <a href="https://www.facebook.com/manas.sharma.mili"><i class="fa fa-facebook"></i></a>
+                    <a href="https://www.instagram.com/manas.sharmaa/"><i class="fa fa-instagram"></i></a>
+                    <a href="mailto:culrav2021@gmail.com"><i class="fa fa-envelope"></i></a>
+                    <a href="tel:+919452460030"><i class="fa fa-phone"></i></a>
+                </div>
+            </div>
+            <div class="celeb-name"><h3>Manas Sharma</h3></div>
+            <div class="celeb-details"></div>
+        </div>
+
+        <div class="celeb">
+            <div class="celeb-image">
+                <div class="glitch-image"><img src="images/team/Shivangi.png"></div>
+                <div class="celeb-work">Festival Secretary</div>
+                <div class="celeb-social-links">
+                    <a href="https://www.facebook.com/shivangi.jha.792"><i class="fa fa-facebook"></i></a>
+                    <a href="https://www.instagram.com/ishivangiiiiii/"><i class="fa fa-instagram"></i></a>
+                    <a href="mailto:culrav2021@gmail.com"><i class="fa fa-envelope"></i></a>
+                </div>
+            </div>
+            <div class="celeb-name"><h3>Shivangi Jha</h3></div>
+        </div>
+
+        <div class="celeb">
+            <div class="celeb-image">
+                <div class="glitch-image"><img src="images/team/Vikram.png"></div>
+                <div class="celeb-work">Festival Secretary</div>
+                <div class="celeb-social-links">
+                    <a href="mailto:culrav2021@gmail.com"><i class="fa fa-envelope"></i></a>
+                    <a href="tel:+917260950476"><i class="fa fa-phone"></i></a>
+                </div>
+            </div>
+            <div class="celeb-name"><h3>Vikram Pratap Singh</h3></div>
+        </div>
+    </div>
+
+    
+    <center style="margin:50px 0"><h1><span>Design Team</span></h1></center>
+    <div class="flex" style="margin-top:0;">
+    
+        <div class="celeb">
+            <div class="celeb-image">
+                <div class="glitch-image" style="filter:unset;"><img src="images/team/Nilotpal.png"></div>
+                <div class="celeb-work">Design Team</div>
+                <div class="celeb-social-links">
+                    <a href="https://www.facebook.com/nilotpal.mukherjee.50"><i class="fa fa-facebook"></i></a>
+                    <a href="https://www.instagram.com/_nilotpal_404"><i class="fa fa-instagram"></i></a>
+                    <a href="tel:+919297848673"><i class="fa fa-phone"></i></a>
+                </div>
+            </div>
+            <div class="celeb-name"><h3>Nilotpal Mukherjee</h3></div>
+        </div>
+
+    </div>
+
+    <center style="margin:50px 0">
+        <h1>
+            <span>Web Team</span>
+        </h1>
+    </center>
+
+    <div class="flex" style="margin-top:0;">
+
+        <div class="celeb" style="width: 12%">
+            <div class="celeb-image">
+                <div class="glitch-image"><img src="images/team/Abhay.png"></div>
+                <div class="celeb-work">Web Team</div>
+                <div class="celeb-social-links">
+                    <a href="https://www.instagram.com/katheriaabhay/"><i class="fa fa-instagram"></i></a>
+                    <a href="tel:+917055550124"><i class="fa fa-phone"></i></a>
+                </div>
+            </div>
+            <div class="celeb-name"><h3>Abhay Katheria</h3></div>
+        </div>
+
+        <div class="celeb" style="width: 12%">
+            <div class="celeb-image">
+                <div class="glitch-image"><img src="images/team/Arpit.png"></div>
+                <div class="celeb-work">Web Team</div>
+                <div class="celeb-social-links">
+                    <a href="https://www.instagram.com/arpitbaranwaal/"><i class="fa fa-instagram"></i></a>
+                    <a href="tel:+917571819104"><i class="fa fa-phone"></i></a>
+                </div>
+            </div>
+            <div class="celeb-name"><h3>Arpit Anand</h3></div>
+        </div>
+        
+        <div class="celeb" style="width: 12%">
+            <div class="celeb-image">
+                <div class="glitch-image"><img src="images/team/Himanshu.png"></div>
+                <div class="celeb-work">Web Team</div>
+                <div class="celeb-social-links">
+                    <a href="https://www.instagram.com/he_man_shu180599/"><i class="fa fa-instagram"></i></a>
+                    <a href="tel:+917086040412"><i class="fa fa-phone"></i></a>
+                </div>
+            </div>
+            <div class="celeb-name"><h3>Himanshu</h3></div>
+        </div>
+        
+        <div class="celeb" style="width: 12%">
+            <div class="celeb-image">
+                <div class="glitch-image"><img src="images/team/Shubham.png"></div>
+                <div class="celeb-work">Web Team</div>
+                <div class="celeb-social-links">
+                    <a href="https://www.instagram.com/deejhavoo/"><i class="fa fa-instagram"></i></a>
+                    <a href="tel:+916901136751"><i class="fa fa-phone"></i></a>
+                </div>
+            </div>
+            <div class="celeb-name"><h3>Shubham K. Jha</h3></div>
+        </div>
+        
+        <div class="celeb" style="width: 12%">
+            <div class="celeb-image">
+                <div class="glitch-image"><img src="images/team/Shubham2.png"></div>
+                <div class="celeb-work">Web Team</div>
+                <div class="celeb-social-links">
+                    <a href="https://www.instagram.com/kshubham175/"><i class="fa fa-instagram"></i></a>
+                    <a href="tel:+917318227753"><i class="fa fa-phone"></i></a>
+                </div>
+            </div>
+            <div class="celeb-name"><h3>Shubham Kumar</h3></div>
+        </div>
+        
+        <div class="celeb" style="width: 12%">
+            <div class="celeb-image">
+                <div class="glitch-image"><img src="images/team/Vishwajeet.png"></div>
+                <div class="celeb-work">Web Team</div>
+                <div class="celeb-social-links">
+                    <a href="https://www.instagram.com/vishu_1208/"><i class="fa fa-instagram"></i></a>
+                    <a href="tel:+918340782670"><i class="fa fa-phone"></i></a>
+                </div>
+            </div>
+            <div class="celeb-name"><h3>Vishwajeet Kumar</h3></div>
+        </div>
+        
+
+    </div>
+
+</section>
+<?php
+if(($_SERVER['REQUEST_METHOD'] === 'GET') or (isset($_POST['action']) and $_POST['action'] != "getContentPage")){
+    include_once('components/footer.php');
+}
+?>
